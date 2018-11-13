4ed37476aa09ab4356ec7f9676d28abb42ffefcb
Dana <doneil1580@gmail.com>
2018-11-12 23:39:15 -0500

    dashboard is set up however still needs some styling and will need the magical ruby added

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { registrations: 'registrations', confirmation: 'confirmations' }
   
-  root to: "pages#home"	
+  root to: "pages#index"	
 end
